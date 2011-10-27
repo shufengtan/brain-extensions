9f7f2cb441099fad4dc94b6f192613d2cc3fd00e
Shufeng Tan <shufengtan@gmail.com>
2011-10-26 22:30:12 -0400

    Found unique solution for the 32x32 problem in 15 iterations

--- python_challenge_32.py
+++ python_challenge_32.py
@@ -2,94 +2,156 @@ from backtracking import backtracking
 import re
 
 Choices = [' ', 'x']
+cluster_re = re.compile(filter(lambda x: x != ' ',  Choices)[0] + '+')
+
+def count_clusters(s):
+    return map(len, cluster_re.findall(''.join([Choices[s[k]] for k in sorted(s.keys())])))
 
 def hv_counts(data):
     given = [map(int, line.split()) for line in data.splitlines() if re.search(r'\d', line)]
     dim = given[0]
     assert len(given) == sum(dim) + 1
-    h_counts = given[1:1+dim[1]]
-    v_counts = given[1+dim[1]:]
-    return h_counts, v_counts
+    d_i_c =  [((0, i), c) for i, c in enumerate(given[1:1+dim[1]])]
+    d_i_c += [((1, i), c) for i, c in enumerate(given[1+dim[1]:])]
+    return dict(d_i_c)
 
-def search():
-    that_idx = None     # index for THAT dimension
-    this_counts = None  # counts for THIS dimension
-    this_dim = None     # len of THIS dimension
-    this_sol = None # choices in THIS dimension, len == 0 or this_dim
-    def counts_assignable(cpoint, choice, sol):
-        if len(this_sol) > cpoint and Choices[choice] not in set([c[that_idx] for c in this_sol[cpoint]]):
+def assignments(m, n):
+    '''Calc how many ways of putting m items in n boxes'''
+    if m == 1:
+        return n
+    if n <= 1:
+        return n
+    return sum(assignments(m - i, n - 1) for i in range(m+1))
+
+def dof(counts, dim):
+    '''Calculate degree of freedom'''
+    lc = len(counts)
+    return(assignments(dim - (sum(counts) + lc - 1), lc + 1))
+
+class PC32:
+    def __init__(self, data):
+        given = [map(int, line.split()) for line in data.splitlines() if re.search(r'\d', line)]
+        dim = given[0]
+        assert len(given) == sum(dim) + 1
+        d_i_c =  [((0, i), c) for i, c in enumerate(given[1:1+dim[1]])]
+        d_i_c += [((1, i), c) for i, c in enumerate(given[1+dim[1]:])]
+        hv_counts = dict(d_i_c)
+        self.dim = dim
+        self.hv_counts = hv_counts
+        self.dof = dict([(d_i, dof(hv_counts[d_i], dim[d_i[0]])) for d_i in hv_counts])
+        self.candidates = {}
+        self.debug = False
+    def box_assignable(self, cpoint, choice, sol):
+        '''This method determines if a choice satisfies all constraints for the current solution.'''
+        this_d_i = self.d_i
+        this_counts = self.hv_counts[this_d_i]
+        sol2 = dict(sol)
+        sol2[cpoint] = choice
+        sol_counts = count_clusters(sol2)
+        this_dim = self.dim[this_d_i[0]]
+        that_d = 1 - this_d_i[0]
+        that_dim = self.dim[that_d]
+        debug = self.debug
+        if len(sol_counts) > len(this_counts):
+            if debug:
+                print '#', this_d_i, cpoint, choice, 'return A false', sol_counts, this_counts
             return False
-        if cpoint in sol:
-            return sol[cpoint] == choice
+        boxes_left = that_dim - cpoint - 1
+        if len(sol_counts) > 0:
+            if max(sol_counts) > max(this_counts):
+                if debug:
+                    print '#', this_d_i, cpoint, choice, 'return B false'
+                return False
         else:
-            sol2 = dict(sol)
-            sol2[cpoint] = choice
-            sol_counts = map(len, re.findall(r'x+', ''.join([Choices[sol2[k]] for k in sorted(sol2.keys())])))
-            if len(sol_counts) > len(this_counts):
+            boxes_needed = sum(this_counts) + len(this_counts) - 1
+            if boxes_left < boxes_needed:
+                if debug:
+                    print '#', this_d_i, cpoint, choice, 'return C false', boxes_left, boxes_needed
                 return False
-            if len(sol_counts) > 0 and max(sol_counts) > max(this_counts):
-                return False
-            if len(sol2) == this_dim:
-                return sol_counts == this_counts
-            if 0 in sol2:
-                sol_this_counts = zip(sol_counts[:-1], this_counts)
-                if len(sol_this_counts) >= 1:
-                    #print '##', sol_this_counts
-                    for c1, c2 in sol_this_counts:
-                        #print '# that idx %d: %d, %d' % (that_idx, c1, c2)
-                        if c1 != c2:
-                            return False
-            else:
-                print "??", sol2
-            return True
-    h_sol = []
-    v_sol = []
-    for itr in xrange(10):
-        print 'iteration', itr
-        v_sol = []
-        for v_idx in range(V_dim):
-            # this is horizonal
-            that_idx = v_idx
-            this_counts = H_counts[v_idx]
-            this_dim = H_dim
-            this_sol = h_sol
-            ss = []
-            for hc in backtracking(range(H_dim), Choices, counts_assignable):
-                print 'V %d |%s|' % (v_idx, ''.join([hc[x] for x in range(H_dim)]))
-                ss.append(hc)
-            v_sol.append(ss)
-        print 'v_sol', map(len, v_sol)
-        for vs in v_sol:
-            if len(vs) > 1:
+        if 0 in sol2:
+            sol_this_counts = zip(sol_counts, this_counts)
+            if len(sol_this_counts) > 0:
+                if sol_this_counts[-1][0] > sol_this_counts[-1][1]:
+                    # last count in sol already > last count in constraint
+                    if debug:
+                        print '#', this_d_i, cpoint, choice, 'return D false'
+                    return False
+                elif sol_this_counts[-1][0] != sol_this_counts[-1][1] and Choices[choice] == ' ':
+                    if debug:
+                        print '#', this_d_i, cpoint, choice, 'return E false'
+                    return False
+            if len(sol_this_counts) > 1:
+                for c1, c2 in sol_this_counts[:-1]:
+                    if c1 != c2:
+                        if debug:
+                            print '#', this_d_i, cpoint, choice, 'return F false'
+                        return False
+            if sol_counts:
+                boxes_needed = sol_this_counts[-1][1] - sol_this_counts[-1][0]
+                if len(sol_counts) < len(this_counts):
+                    this_counts_left = this_counts[len(sol_counts):]
+                    boxes_needed += sum(this_counts_left) + len(this_counts_left) - 1
+                if boxes_left < boxes_needed:
+                    if debug:
+                        print '#', this_d_i, cpoint, choice, 'return G false', boxes_left, boxes_needed
+                    return False
+        candidates = self.candidates
+        if (that_d, cpoint) in candidates:
+            c_list = map(lambda c: c[this_d_i[1]], candidates[(that_d, cpoint)])
+            if Choices[choice] not in c_list:
+                 if debug:
+                     print '#', this_d_i, cpoint, choice, 'return H false', that_d
+                     for c in candidates[(that_d, cpoint)]:
+                         print '|%s|' % ''.join([c[i] for i in xrange(len(c))])
+                 return False
+        if len(sol2) == that_dim:
+            res = sol_counts == this_counts
+            if debug:
+                print "#", this_d_i, cpoint, choice, 'return I', res
+            return res
+        if debug:
+            print '#', this_d_i, cpoint, choice, 'fall through True', this_counts, sol_counts
+        return True
+    def solve(self, d_i_list):
+        for d_i in d_i_list:
+            print 'DoF', d_i, self.hv_counts[d_i], self.dof[d_i]
+        dim = self.dim
+        candidates = self.candidates
+        cand_counts = [None, None]
+        prev_cand_counts = [None, None]
+        for itr in xrange(1000):
+            print 'Iteration', itr
+            for d_i in d_i_list:
+                self.d_i = d_i
+                this_dim = dim[d_i[0]]
+                that_dim = dim[1 - d_i[0]]
+                this_idx = d_i[1]
+                self.candidates[d_i] = []
+                for candidate in backtracking(range(this_dim), Choices, self.box_assignable):
+                    if candidate not in self.candidates[d_i]:
+                        self.candidates[d_i].append(candidate)
+                print 'D%d %2d %d/%d' % (d_i[0], d_i[1], len(self.candidates[d_i]), self.dof[d_i])
+            for d in (0, 1):
+                cand_counts[d] = [len(self.candidates[(d, idx)]) for idx in xrange(dim[1-d])]
+            if cand_counts[0] == prev_cand_counts[0] and cand_counts[1] == prev_cand_counts[1]:
+                print 'Candidate counts remain the same. Stop.', cand_counts
                 break
-        else:
-            print "Found unique solution:"
-            for vs in v_sol:
-                print ''.join([vs[0][x] for x in range(H_dim)])
-            return
-        h_sol = []
-        for h_idx in range(H_dim):
-            # this is vertical
-            that_idx = h_idx
-            this_counts = V_counts[h_idx]
-            this_dim = V_dim
-            this_sol = v_sol
-            ss = []
-            for vc in backtracking(range(V_dim), Choices, counts_assignable):
-                print 'H %d |%s|' % (h_idx, ''.join([vc[y] for y in range(V_dim)]))
-                ss.append(vc)
-            h_sol.append(ss)
+            for d in (0, 1):
+                prev_cand_counts[d] = cand_counts[d]
+            if len(set(cand_counts[0])) == 1 and cand_counts[0][0] == 1:
+                print '** Found unique solution'
+                break
+        for y in xrange(dim[1]):
+            print ''.join([candidates[(0, y)][0][x] for x in xrange(dim[0])])
 
 if __name__ == '__main__':
     import sys
     data_file = sys.argv[1]
+    #data_file = 'pc32_2.txt'
     fh = open(data_file)
     data = fh.read()
     fh.close()
-    H_counts, V_counts = hv_counts(data)
-    H_dim = len(V_counts)
-    V_dim = len(H_counts)
-    print 'H dim', H_dim, 'V dim', V_dim
-    print 'H counts:', H_counts
-    print 'V counts:', V_counts
-    search()
+    pc32 = PC32(data)
+    d_i_list = pc32.hv_counts.keys()
+    d_i_list.sort(key=pc32.dof.get)
+    pc32.solve(d_i_list)
